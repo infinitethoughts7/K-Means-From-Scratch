57fe9f7439e606e6078a128467b034166b34a45c
Rocky <rakeshganji99@gmail.com>
2023-09-30 09:45:40 +0530

    k.py

--- k.py
+++ k.py
@@ -16,15 +16,6 @@ X2, y2 = make_blobs(n_samples=100, random_state=123)
 # X1 and X2 will be different because of the different random states
 In this example, X1 and X2 will be different because we used different values for random_state. If we had used the same value for random_state, X1 and X2 would be the same.
 '''
-print(type(x_train))
-print(x_train.shape)
-
-df = DataFrame(dict(x= x_train[:, 0], y= x_train[:,1]))
-fig, ax = plt.subplots(figsize =(8,8))
-df.plot(ax =ax , kid = 'Scatter', x = "x", y = 'y')
-plt.xlabel('x_1')
-plt.ylabel('x_2')
-pot.show()
 
 
 
